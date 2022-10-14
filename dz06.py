e2b6f354e59d9b1085edf8d20fea8ed5b4dd03a3
aleksndr <ostraleks@gmail.com>
2022-10-14 22:05:05 +0500

    14.10/22.04

--- dz06.py
+++ dz06.py
@@ -14,13 +14,33 @@
 # [28, 20, 10, 5, 1, 24, 7, 15, 23, 25]
 # [24, 15, 23, 25]
 
-from random import randint
-def new_list(num = int(input("Введите количество цифр "))):
-    list = [randint(1, 100) for _ in range(num)]
-    print(f'Входящий список:\n{list}')
-    return list
-def out_list(in_list = new_list()):
-    out_l = [in_list[i] for i in range(1, len(in_list)) if int(in_list[i - 1]) < int(in_list[i])]
-    return out_l
+# from random import randint
+# def new_list(num = int(input("Введите количество цифр "))):
+#     list = [randint(1, 100) for _ in range(num)]
+#     print(f'Входящий список:\n{list}')
+#     return list
+# def out_list(in_list = new_list()):
+#     out_l = [in_list[i] for i in range(1, len(in_list)) if int(in_list[i - 1]) < int(in_list[i])]
+#     return out_l
 
-print(f'Исходящий список:\n{out_list()}')
+# print(f'Исходящий список:\n{out_list()}')
+
+
+
+# 2. Для чисел в пределах от 20 до N найти числа, кратные 20 или 21. Use comprehension.
+# in
+# 100
+#
+# out
+# [20, 21, 40, 42, 60, 63, 80, 84, 100]
+#
+# in
+# 424
+#
+# out
+# [20, 21, 40, 42, 60, 63, 80, 84, 100, 105, 120, 126, 140, 147, 160, 168, 180, 189, 200, 210,
+#  220, 231, 240, 252, 260, 273, 280, 294, 300, 315, 320, 336, 340, 357, 360, 378, 380, 399, 400, 420]
+
+
+in_list = [i for i in range(20, int(input("Введите конечное число "))+1)]
+print(list(x for x in in_list if x % 20 == 0 or x % 21 == 0))
